034755b7416ead361fe64c4cdc159940d3405463
PLANFIAMX\gcasimiro <gcasimiro@planfia.com>
2019-04-02 14:25:55 -0600

    Optimizacion de configuracion de yargs

--- modulo4/app.js
+++ modulo4/app.js
@@ -3,30 +3,7 @@ const {
     crearArchivo,
     mostrar
 } = require('./multiplicar/multiplicar');
-const argv = require('yargs')
-    .command('listar', 'Imprime en consola la tabla de multiplicar', {
-        base: {
-            demand: true,
-            alias: 'b'
-        },
-        limite: {
-            alias: 'l',
-            default: 10
-        }
-
-    })
-    .command('crear', 'Crea un archivo con el resultado', {
-        base: {
-            demand: true,
-            alias: 'b'
-        },
-        limite: {
-            alias: 'l',
-            default: 10
-        }
-    })
-    .help()
-    .argv;
+const { argv } = require('./config/yargs');
 
 
 
