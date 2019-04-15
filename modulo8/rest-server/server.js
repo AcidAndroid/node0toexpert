c6833d47bbbe9f2c9cdf893bedc75b86b08b63dd
PLANFIAMX\gcasimiro <gcasimiro@planfia.com>
2019-04-15 17:11:34 -0500

    Se hace ejmplo de express con peticiones CRUD y resuesta en status para diferentes tipos de respuestas

---
+++ modulo8/rest-server/server.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+const port = process.env.PORT | 3000
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.get('/', (req, res) => {
+    res.json('Hola')
+})
+
+app.get('/usuario', (req, res) => {
+    res.json('get Gustavo')
+})
+
+app.post('/usuario/', (req, res) => {
+
+
+    let body = req.body
+
+    if (!body.nombre) {
+        res.status(400).json({
+            messase: 'No se proporciono un nombre'
+        })
+    } else {
+        console.log(`Body enviado`, body);
+        res.json({ usuario: body })
+    }
+
+
+})
+
+app.put('/usuario/:id', (req, res) => {
+    let id = req.params.id
+    res.json({ id })
+})
+
+app.delete('/usuario/:id', (req, res) => {
+    res.json('delete Gustavo')
+})
+
+app.listen(port, () => {
+    console.log(`Escuchando en el puerto ${port}`);
+})
