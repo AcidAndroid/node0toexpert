b3e3d501a48a99cd9eb46390f28bd3f58da4b101
PLANFIAMX\gcasimiro <gcasimiro@planfia.com>
2019-04-26 12:05:15 -0500

    Se separa las rutas de usaurios del server

--- modulo9/server.js
+++ modulo9/server.js
@@ -1,15 +1,17 @@
 const mongoose = require('mongoose');
+const express = require('express');
+const app = express();
 
 
 
-const bodyParser = require('body-parser');
 require('./config/config')
 
 //Ahora se maneja en un archivo de configuracion
 // const port = process.env.PORT | 3000
-
+const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+app.use(require('./rutas/usuario'))
 
 
 
