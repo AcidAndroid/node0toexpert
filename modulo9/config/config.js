1649b5f8fe6fb67e57f9be2ea07957f63c8ab65d
PLANFIAMX\gcasimiro <gcasimiro@planfia.com>
2019-04-29 16:47:24 -0500

    Se agregan varialbes de entorno para desarrollo y produccion

--- modulo9/config/config.js
+++ modulo9/config/config.js
@@ -1,1 +1,15 @@
 process.env.PORT = process.env.PORT || 3000
+
+/**
+ * Entorno
+ */
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev'
+
+/**Base de datos */
+let urlDB
+if (process.env.NODE_ENV == 'dev') {
+    urlDB = 'mongodb://192.168.56.102:27017/cafe'
+} else {
+    urlDB = 'mongodb://mongo:mongomon9@ds145456.mlab.com:45456/cafe'
+}
+process.env.URLDB = urlDB
