1b46ca3d74aaa1f67f5aba6496892f0ab49009b3
kelldintch <kelldintch@hotmail.com>
2019-05-12 19:07:00 -0500

    Carga de archivos y cambio de nombre para evitar sobrescrituras

--- modulo9/rutas/uploads.js
+++ modulo9/rutas/uploads.js
@@ -8,20 +8,40 @@ ruta.use(file({
     useTempFiles: true
 }))
 
-ruta.put('/upload', (req, res) => {
+ruta.put('/upload/:tipo/:id', (req, res) => {
 
-    let extensionesValidas=['pdf','png','jpeg','jpg','bmp']
-    let nombreArchivo= req.files.archivo.name
-    let extensionPosicion =nombreArchivo.split('.').length
+    let tipo = req.params.tipo
+    let id = req.params.id
 
-    let extensionNormal= nombreArchivo.split('.')[extensionPosicion-1]
-    extensionPosicion =req.files.archivo.mimetype.split('/').length
-    let extensionMime= req.files.archivo.mimetype.split('/')[extensionPosicion-1]
-    
+
+    //Tipos de objetos que permiten carga de archivos
+    let tipoValidos = ['usuario', 'producto']
+
+    //Tipos de extensiones validas
+    let extensionesValidas = ['pdf', 'png', 'jpeg', 'jpg', 'bmp']
+    let nombreArchivo = req.files.archivo.name
+    let extensionPosicion = nombreArchivo.split('.').length
+
+    let extensionNormal = nombreArchivo.split('.')[extensionPosicion - 1]
+    extensionPosicion = req.files.archivo.mimetype.split('/').length
+    let extensionMime = req.files.archivo.mimetype.split('/')[extensionPosicion - 1]
+
+
+    //Obtencion del reques de los archivos cargados
+    let archivo = req.files.archivo
 
     console.log(extensionNormal);
     console.log(extensionMime);
+    console.log(tipo)
 
+    if (!tipoValidos.includes(tipo)) {
+        return res.status(400).json({
+            ok: false,
+            err: {
+                message: 'Tipo de objeto no pemitido para tener archivos '
+            }
+        })
+    }
 
     if (!req.files) {
         return res.status(400).json({
@@ -33,20 +53,25 @@ ruta.put('/upload', (req, res) => {
     }
 
 
-    if(!extensionesValidas.includes(extensionNormal) || !extensionesValidas.includes(extensionMime) ){
+    if (!extensionesValidas.includes(extensionNormal) || !extensionesValidas.includes(extensionMime)) {
         return res.json({
-            ok:false
-            ,err:{
-                message:'Tipo de archivo no permitido'
+            ok: false,
+            err: {
+                message: 'Tipo de archivo no permitido'
             }
         })
     }
 
 
+    //Cambiar nombre al archivo
+    nombreArchivo = `${id} - ${nombreArchivo}-${new Date().getMilliseconds()}.${extensionNormal}`
 
-    let archivo = req.files.archivo
+    console.log(nombreArchivo)
 
-    archivo.mv('uploads/'+ nombreArchivo, (err) => {
+
+    archivo.mv('uploads/' + tipo + '/' + nombreArchivo, (err) => {
+
+        //Erro al cargar el archivo
         if (err) {
             return res.status(500).json({
                 ok: false,
@@ -54,9 +79,10 @@ ruta.put('/upload', (req, res) => {
             });
         }
 
+        //Todo Ok
         return res.status(404).json({
-            ok:true
-            ,message:'Archivo ' + nombreArchivo  + ' cargado'
+            ok: true,
+            message: 'Archivo ' + nombreArchivo + ' cargado'
         })
     })
 });
